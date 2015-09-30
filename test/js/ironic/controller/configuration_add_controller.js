bb47fec13ace17c0681ce22919a313951699ada1
Michael Krotscheck <krotscheck@gmail.com>
2015-09-29 18:53:56 -0700

    Separated UI and Configuration Logic
    This removes the use of UI components from the openstack library,
    making it reusable outside of ironic-webclient. It is mainly achieved by
    removing the addLocal invocation of $modal from $$configuration, nad
    moving the ConfigurationAddController from the openstack module
    to the ironic root application module. Tests for the
    ConfigurationAddController have been also added, and the test
    threshold has been raised accordingly.
    
    Change-Id: Icfd7504be966cee330a934c99b50465fd8ff7993

---
+++ test/js/ironic/controller/configuration_add_controller.js
@@ -0,0 +1,98 @@
+/**
+ * Unit tests for the application root module.
+ */
+describe('Unit: Ironic-webclient Add-Configuration Controller',
+  function() {
+    'use strict';
+
+    var $controller;
+    var $scope = {};
+    var mockInjectionProperties = {
+      $scope: {},
+      $modalInstance: {
+        close: function() {
+        },
+        dismiss: function() {
+        }
+      },
+      configuration: [{id: 'test'}]
+    };
+
+    beforeEach(function() {
+      // Load the ironic module.
+      module('ironic');
+
+      // Load the controller provider.
+      inject(function(_$controller_) {
+        $controller = _$controller_;
+      });
+    });
+
+    describe('Controller Properties', function() {
+      it('does not pollute the $scope', function() {
+        var $scope = {};
+        $controller('ConfigurationAddController', mockInjectionProperties);
+        expect($scope).toEqual({});
+      });
+
+      it('sets the configuration property to the injected configuration',
+        function() {
+          var controller = $controller('ConfigurationAddController', mockInjectionProperties);
+          expect(controller.configuration).toBe(mockInjectionProperties.configuration);
+        });
+
+      it('sets the newConfiguration property to a clone of the $$defaultConfiguration',
+        inject(function($$defaultConfiguration) {
+          var controller = $controller('ConfigurationAddController', mockInjectionProperties);
+          expect(controller.newConfiguration).toEqual($$defaultConfiguration);
+        }));
+
+      it('sets the $scope.location to a hash of the current URL',
+        inject(function($location) {
+          spyOn($location, 'host').and.returnValue('example.com');
+          spyOn($location, 'protocol').and.returnValue('https');
+          spyOn($location, 'port').and.returnValue(1234);
+
+          var controller = $controller('ConfigurationAddController', mockInjectionProperties);
+          expect(controller.location).toEqual({
+            'host': 'example.com',
+            'protocol': 'https',
+            'port': 1234
+          });
+        }));
+    });
+
+    describe('$scope.close', function() {
+      it('calls dismiss when close() is called.',
+        function() {
+          var spy = spyOn(mockInjectionProperties.$modalInstance, 'dismiss');
+          var controller = $controller('ConfigurationAddController', mockInjectionProperties);
+          controller.close();
+          expect(spy).toHaveBeenCalled();
+          expect(spy.calls.count()).toEqual(1);
+        });
+    });
+
+    describe('$scope.save', function() {
+      it('resolves the modal promise with a valid configuration when close() is called',
+        function() {
+          var spy = spyOn(mockInjectionProperties.$modalInstance, 'close');
+          var controller = $controller('ConfigurationAddController', mockInjectionProperties);
+
+          // Simulate form input
+          controller.newConfiguration.name = 'Test Name';
+          controller.save();
+          expect(spy).toHaveBeenCalled();
+
+          // The ID should have been automatically populated from the name, in order to ensure
+          // the existence of a key.
+          expect(spy.calls.mostRecent().args[0]).toEqual({
+            id: 'Test Name',
+            name: 'Test Name',
+            ironic: {
+              api: ''
+            }
+          });
+        });
+    });
+  });
