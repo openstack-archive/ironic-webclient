37672d08141024d6f2d05bd87d88f8851ecfa8dd
Michael Krotscheck <krotscheck@gmail.com>
2016-03-31 13:39:02 -0700

    Added capitalize filter
    This filter capitalizes all words in the passed string. It is
    intended as a temporary shim for string formatting, until we can
    get i18n incorporated.
    
    Change-Id: I0a7b0d61d27d24cb2ccf2a2ff2757097cab10ba7

--- karma.conf.js
+++ karma.conf.js
@@ -72,7 +72,7 @@
       thresholdReporter: {
         statements: 93, // target 100
         branches: 96, // target 100
-        functions: 89, // target 100
+        functions: 90, // target 100
         lines: 93 // target 100
       },
 
